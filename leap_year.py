2bc35422a735837907c9942c3151da7d738a1b9c
LucianoRenkine <162723184+LucianoRenkine@users.noreply.github.com>
2024-04-12 12:51:12 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,12 @@
 def leap_year():
-    print("TO DO")
+    Año = int(input("Ingrese un año: "))
+    if Año%100==0:
+        if Año%400==0:
+            print(f"El año {Año} es bisiesto")
+        else:
+            print(f"El año {Año} no es bisiesto")
+    else:
+        if Año%4==0:
+            print(f"El año {Año} es bisiesto")
+        else:
+            print(f"El año {Año} no es bisiesto")
